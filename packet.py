4947b67303edd8ac185cf07a37ac06c24756d9e4
HamzaHYSA <98532366+HamzaHYSA@users.noreply.github.com>
2023-05-08 21:01:37 -0400

    Update packet.py

--- packet.py
+++ packet.py
@@ -9,107 +9,67 @@ import random
 import asyncore
 import numpy as np
 
-def create_packet(pkttype, ttl, kval, src, dst, dst1, dst2, dst3, seq, hops, adRoute, LS, CRC, data):
-    """Create a new packet based on given id"""
-    # Type(1),  LEN(4), SRCID(1),  DSTID(1), SEQ(4), DATA(1000)
-    if pkttype == 1: #HELLO PACKET
+def create_packet(pkttype, ttl, kval, src, dst, dst1, dst2, dst3, seq, hops, adRoute, LS, CRC, data=None):
+    """Create a new packet based on given type"""
+
+    if pkttype == 1:  # HELLO PACKET
         header = struct.pack('BBBL', pkttype, seq, ttl, src)
-    if pkttype == 2: #HELLO ACK PACKET
+
+    elif pkttype == 2:  # HELLO ACK PACKET
         header = struct.pack('BBBLL', pkttype, seq, ttl, src, dst)
-    if pkttype == 3: #ROUTING PACKET
+
+    elif pkttype == 3:  # ROUTING PACKET
         header = struct.pack('BBBBBBLLB', pkttype, seq, ttl, pkttype, src, hops, adRoute, LS, CRC)
-    if pkttype == 4: #UNICAST DATA PACKET
+
+    elif pkttype == 4:  # UNICAST DATA PACKET
         header = struct.pack('BBBLL', pkttype, seq, ttl, src, dst)
-        header = header + bytes(data,'utf-8')
-    if pkttype == 5: #MULTICAST DATA PACKET
+        if data:
+            header += bytes(data, 'utf-8')
+
+    elif pkttype == 5:  # MULTICAST DATA PACKET
+        # Attach the unicast header to the front of the multicast packet
+        unicast_header = struct.pack('BBBLL', 4, seq, ttl, src, dst)
         header = struct.pack('BBBBLLL', pkttype, seq, ttl, kval, dst1, dst2, dst3)
-        header = header + bytes(data,'utf-8')
+        if data:
+            header += bytes(data, 'utf-8')
+        header = unicast_header + header
+
+    else:
+        raise ValueError("Invalid packet type")
+
     return header
 
-def read_header(pkt):
-    #Change the bytes to account for network encapsulations
-    header = pkt[0:32]
-    #pktFormat = "BLBBL"
-    #pktSize = struct.calcsize(pktFormat)
-    pkttype, pktlen, dst, src, seq = struct.unpack('BLBBL', header)
-    return pkttype, pktlen, dst, src, seq
 
-def read_data(pkt):
-        #Change the bytes to account for network encapsulations
-    data = pkt[32:]
-    return data
+def read_packet(packet):
+    """Read the header and data fields of a packet"""
 
+    # Unpack the type field to determine the packet type
+    pkttype = struct.unpack('B', packet[0:1])[0]
 
+    # Parse the header based on the packet type
+    if pkttype == 1:  # HELLO PACKET
+        header = struct.unpack('BBBL', packet[0:8])
+        data = None
 
+    elif pkttype == 2:  # HELLO ACK PACKET
+        header = struct.unpack('BBBLL', packet[0:16])
+        data = None
 
-###################################
-####   IGNORE THE SHIT BELOW   ####
-###################################
+    elif pkttype == 3:  # ROUTING PACKET
+        header = struct.unpack('BBBBBBLLB', packet[0:16])
+        data = None
 
-#Starts a ping from current host (src) to desired destination (dst)
-def ping(h, c, dst):
-    seq_num, nor, rtt = 0, 0, []
-    #count = 0
-    for x in range(c):
-        #count += 1
-        # Creates and sends the request packet
-        packet = create_packet(1, h.id, dst, seq=seq_num, data='This is assignment 5!')
-        send_packet(h, packet)
-        send_time = time.time()
+    elif pkttype == 4:  # UNICAST DATA PACKET
+        header = struct.unpack('BBBLL', packet[0:16])
+        data = packet[16:].decode('utf-8')
 
-        # Waits to receive a reply packet to move onto next ping
-        seq_failed = receive_packet(h, packet)
-        if seq_failed == -1:
-            rtt.append(time.time()-send_time)
-            seq_num += 1
-        else:
-            x -= 1
-            nor += 1
-            print("Retransmitting packet with seq num: ", seq_num)
-    rtt = np.array(rtt)
-    #print(count)
-    print(c, " packets transmitted, ", nor, " packets retransmitted, ", (nor/c)*100, "% packet loss",
-         "\n round-trip min/avg/max/stddev = ", np.min(rtt),"/",np.mean(rtt),"/",np.max(rtt),"/",np.std(rtt), " s" )
-    return 0
+    elif pkttype == 5:  # MULTICAST DATA PACKET
+        # Strip off the unicast header before parsing the multicast packet
+        unicast_header = struct.unpack('BBBLL', packet[0:16])
+        header = struct.unpack('BBBBLLL', packet[16:32])
+        data = packet[32:].decode('utf-8')
 
-# Sends a packet across UDP socket the corresponding router gateway for that host
-def send_packet(h, packet):
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.sendto(packet, h.default_gateway)
-    s.close()
-    print("Sending: ", packet, " To: ", h.default_gateway)
-    return 0
+    else:
+        raise ValueError("Invalid packet type")
 
-# Receives packets across UDP socket
-def receive_packet(h, sent_packet):
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.bind((h.ip, h.port))
-    seq_failed = -1
-
-    #Waits to receive packet on h.ip/h.port
-    while True:
-        try:
-            if sent_packet != None:
-                s.settimeout(0.007)
-            packet,addr = s.recvfrom(1024)
-            pkttype, pktlen, dst, src, seq = read_header(packet)
-        except OSError:
-            pkttype, pktlen, dst, src, seq = read_header(sent_packet)
-            seq_failed = seq
-            break
-
-        if(pkttype == 1 and dst == h.id):
-            print("Received: ", packet, " From: ", src)
-
-            # Creates reply packet
-            packet = create_packet(2, h.id, src, 0, 'This is a reply!')
-            send_packet(h, packet)
-
-        # Checks for reply packet (Note this is not very flexable and would break the server if it receives reply packet)
-        elif(pkttype == 2 and dst == h.id):
-            #data = read_data(packet)
-            print("Receved: ", packet, " From: ", src)
-            break
-
-    s.close()
-    return  seq_failed
+    return pkttype, header, data
