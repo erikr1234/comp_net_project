445e8565da5270a0613eab837f169a9c4dd00e92
HamzaHYSA <98532366+HamzaHYSA@users.noreply.github.com>
2023-05-08 23:01:43 -0400

    Update packet.py

--- packet.py
+++ packet.py
@@ -10,24 +10,25 @@ import asyncore
 import numpy as np
 
 def create_packet(pkttype, ttl, kval, src, dst, dst1, dst2, dst3, seq, hops, adRoute, LS, CRC, data=None):
-    """Create a new packet based on given type"""
-
-    if pkttype == 1:  # HELLO PACKET
+    
+# The Packets: Hello, Hello ACK, Routing, Unicast, and Multicast
+    
+    if pkttype == 1:  
         header = struct.pack('BBBL', pkttype, seq, ttl, src)
 
-    elif pkttype == 2:  # HELLO ACK PACKET
+    elif pkttype == 2:  
         header = struct.pack('BBBLL', pkttype, seq, ttl, src, dst)
 
-    elif pkttype == 3:  # ROUTING PACKET
+    elif pkttype == 3:  
         header = struct.pack('BBBBBBLLB', pkttype, seq, ttl, pkttype, src, hops, adRoute, LS, CRC)
 
-    elif pkttype == 4:  # UNICAST DATA PACKET
+    elif pkttype == 4:  
         header = struct.pack('BBBLL', pkttype, seq, ttl, src, dst)
         if data:
             header += bytes(data, 'utf-8')
 
-    elif pkttype == 5:  # MULTICAST DATA PACKET
-        # Attach the unicast header to the front of the multicast packet
+    elif pkttype == 5:  
+        
         unicast_header = struct.pack('BBBLL', 4, seq, ttl, src, dst)
         header = struct.pack('BBBBLLL', pkttype, seq, ttl, kval, dst1, dst2, dst3)
         if data:
@@ -41,30 +42,34 @@ def create_packet(pkttype, ttl, kval, src, dst, dst1, dst2, dst3, seq, hops, adR
 
 
 def read_packet(packet):
-    """Read the header and data fields of a packet"""
+    
 
-    # Unpack the type field to determine the packet type
+    # Unpacks the type field in order to figure out the packet type
+    
     pkttype = struct.unpack('B', packet[0:1])[0]
 
-    # Parse the header based on the packet type
-    if pkttype == 1:  # HELLO PACKET
+    # Based on the packet type, the header will be parsed. Checks for if HELLO, HELLO ACK, ROUTING, UNICAST, or MULTICAST
+    
+    if pkttype == 1:  
         header = struct.unpack('BBBL', packet[0:8])
         data = None
 
-    elif pkttype == 2:  # HELLO ACK PACKET
+    elif pkttype == 2: 
         header = struct.unpack('BBBLL', packet[0:16])
         data = None
 
-    elif pkttype == 3:  # ROUTING PACKET
+    elif pkttype == 3: 
         header = struct.unpack('BBBBBBLLB', packet[0:16])
         data = None
 
-    elif pkttype == 4:  # UNICAST DATA PACKET
+    elif pkttype == 4:
         header = struct.unpack('BBBLL', packet[0:16])
         data = packet[16:].decode('utf-8')
 
-    elif pkttype == 5:  # MULTICAST DATA PACKET
-        # Strip off the unicast header before parsing the multicast packet
+    elif pkttype == 5:
+        
+        # Takes off the unicast header before parsing the multicast packet
+        
         unicast_header = struct.unpack('BBBLL', packet[0:16])
         header = struct.unpack('BBBBLLL', packet[16:32])
         data = packet[32:].decode('utf-8')
